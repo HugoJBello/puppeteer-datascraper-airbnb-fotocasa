834459c0c392cdf82141944691a24033caf5d62e
Hugo J. Bello <hjbello.wk@gmail.com>
2018-12-01 13:00:09 +0100

    fix

--- scrapers/AirbnbBoxScraper.js
+++ scrapers/AirbnbBoxScraper.js
@@ -72,16 +72,21 @@ module.exports = class AirbnbBoxScraper {
     }
 
     async initializePuppeteer() {
-        this.browser = await puppeteer.launch({
-            //executablePath: '/usr/bin/chromium-browser'
-            userAgent: randomUA.generate(),
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
+        if (process.env['RASPBERRY_MODE']) {
+            this.browser = await puppeteer.launch({
+                executablePath: '/usr/bin/chromium-browser',
+                userAgent: randomUA.generate(),
+                headless: true,
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+            });
+        } else {
+            this.browser = await puppeteer.launch({
+                userAgent: randomUA.generate(),
+                headless: true,
+                args: ['--no-sandbox']
+            });
+        }
         this.page = await this.browser.newPage();
-        //const devices = require('puppeteer/DeviceDescriptors');
-        //const iPhone = devices['iPhone 6'];
-        //this.page.emulate(iPhone);
     }
 
     async anyResultsFound() {
